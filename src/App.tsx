0abf004999f3eb112c756ad24c019873911b41c1
The SunShining <thesunshiningmin@gmail.com>
2025-02-13 21:44:27 -0700

    path works great!

--- src/App.tsx
+++ src/App.tsx
@@ -3,7 +3,7 @@ import SearchBar from './components/SearchBar/SearchBar'
 import Web4Content from './components/Web4Content/Web4Content'
 import { Routes, Route, useNavigate } from 'react-router-dom'
 
-function App() {
+function HomePage() {
   const navigate = useNavigate()
 
   const handleSearch = (accountId: string) => {
@@ -14,13 +14,17 @@ function App() {
     <div className="app-container">
       <h1 className="title">Sleet Browser</h1>
       <SearchBar onSearch={handleSearch} />
-      <div className="content-frame">
-        <Routes>
-          <Route path="/app/:accountId" element={<Web4Content />} />
-          <Route path="/" element={<div>Enter a NEAR account to view content</div>} />
-        </Routes>
-      </div>
+      <div className="home-message">Enter a NEAR account to view content</div>
     </div>
+  )
+}
+
+function App() {
+  return (
+    <Routes>
+      <Route path="/" element={<HomePage />} />
+      <Route path="/app/:accountId" element={<Web4Content />} />
+    </Routes>
   )
 }
 
